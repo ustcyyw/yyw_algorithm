d9fcf801b0ff84fc1bfe5b8e3c4c71dc821759a7
ustcyyw <yang0@mail.ustc.edu.cn>
2020-03-24 21:12:40 +0800

    237. 删除链表中的节点 2020年3月24日21:12:38

---
+++ easy/LinkedList/deleteNode.java
@@ -0,0 +1,50 @@
+package easy.linkedList;
+
+import BaseClass.ListNode;
+
+/**
+ * @Time : 2020年3月23日19:24:35
+ * @Author : yyw@ustc
+ * @E-mail : yang0@mail.ustc.edu.cn
+ * @Github : https://github.com/ustcyyw
+ * @desc : 已总结
+ */
+
+/**
+ * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+ * 现有一个链表 -- head = [4,5,1,9]，它可以表示为: 
+ *
+ * 示例 1:
+ * 输入: head = [4,5,1,9], node = 5
+ * 输出: [4,1,9]
+ * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+ *
+ * 示例 2:
+ * 输入: head = [4,5,1,9], node = 1
+ * 输出: [4,5,9]
+ * 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ *  
+ *
+ * 说明:
+ * 链表至少包含两个节点。
+ * 链表中所有节点的值都是唯一的。
+ * 给定的节点为非末尾节点并且一定是链表中的一个有效节点。
+ * 不要从你的函数中返回任何结果。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/delete-node-in-a-linked-list
+ */
+public class deleteNode {
+    /**
+     * 执行用时 :0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗 :38.5 MB, 在所有 Java 提交中击败了5.07%的用户
+     */
+    public void deleteNode(ListNode node) {
+        while(node.next.next != null){
+            node.val = node.next.val;
+            node = node.next;
+        }
+        node.val = node.next.val;
+        node.next = null;
+    }
+}
