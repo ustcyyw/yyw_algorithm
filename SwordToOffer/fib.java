b36cb24ded38b187d09aa2e8da95130c3250119f
ustcyyw <yang0@mail.ustc.edu.cn>
2020-04-14 23:52:00 +0800

    面试题10- I. 斐波那契数列 2020年4月14日23:51:59

---
+++ SwordToOffer/fib.java
@@ -0,0 +1,43 @@
+package SwordToOffer;
+
+/**
+ * @Time : 2020年4月14日11:22:35
+ * @Author : yyw@ustc
+ * @E-mail : yang0@mail.ustc.edu.cn
+ * @Github : https://github.com/ustcyyw
+ * @desc :
+ */
+
+/**
+ * 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项。斐波那契数列的定义如下：
+ * F(0) = 0,   F(1) = 1
+ * F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+ * 斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ *
+ * 示例 1：
+ * 输入：n = 2
+ * 输出：1
+ *
+ * 示例 2：
+ * 输入：n = 5
+ * 输出：5
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof
+ */
+public class fib {
+    /**
+     * 执行用时 :0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗 :36.5 MB, 在所有 Java 提交中击败了100.00%的用户
+     */
+    public int fib(int n) {
+        if(n == 0)
+            return 0;
+        int[] result = new int[n + 1];
+        result[1] = 1;
+        for(int i = 2; i <= n; i++)
+            result[i] = (result[i - 2] + result[i - 1])  % 1000000007;
+        return result[n];
+    }
+}
